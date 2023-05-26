0720a3248a89e4ad18798365c68732db789484ea
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-26 03:32:17 -0300

    Boton desplega un menu , implementacion de js

---
+++ controlador/productos.js
@@ -0,0 +1,12 @@
+function show_menu(){
+    document.getElementById('submenu').style.display='block';
+   document.getElementById('icon_menu').style.display='block';
+}
+
+function hide_menu(){
+    document.getElementById('boton').style.display='block';
+    document.getElementById('submenu').style.display='none';
+    document.getElementById('icon_menu').style.display='block';
+}
+
+//
